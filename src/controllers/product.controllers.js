ce7bdafe6ad2fbf61d577fb97a69ec7c7d9b34e1
T.Otiniano <totivas@gmail.com>
2023-07-03 22:31:34 -0500

    Update class Lunes

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,58 +1,59 @@
-const catchError = require('../utils/catchError');
-const Product = require('../models/Product');
-const Category = require('../models/Category');
-const ProductImg = require('../models/ProductImg');
+const catchError = require("../utils/catchError");
+const Product = require("../models/Product");
+const Category = require("../models/Category");
+const ProductImg = require("../models/ProductImg");
 
-const getAll = catchError(async(req, res) => {
-    const {category} =req.query
-    const where ={}
-    if(category) where.categoryId =category
+const getAll = catchError(async (req, res) => {
+    const { category } = req.query;
+    const where = {};
+    if (category) where.categoryId = category;
     const results = await Product.findAll({
-        include:[Category,ProductImg],
-        where
+        include: [Category, ProductImg],
+        where,
     });
     return res.json(results);
 });
 
-const create = catchError(async(req, res) => {
+const create = catchError(async (req, res) => {
     const result = await Product.create(req.body);
     return res.status(201).json(result);
 });
 
-const getOne = catchError(async(req, res) => {
+const getOne = catchError(async (req, res) => {
     const { id } = req.params;
-    const result = await Product.findByPk(id,{include:[Category,ProductImg]});
-    if(!result) return res.sendStatus(404);
+    const result = await Product.findByPk(id, {
+        include: [Category, ProductImg],
+    });
+    if (!result) return res.sendStatus(404);
     return res.json(result);
 });
 
-const remove = catchError(async(req, res) => {
+const remove = catchError(async (req, res) => {
     const { id } = req.params;
-    await Product.destroy({ where: {id} });
+    await Product.destroy({ where: { id } });
     return res.sendStatus(204);
 });
 
-const update = catchError(async(req, res) => {
+const update = catchError(async (req, res) => {
     const { id } = req.params;
-    const result = await Product.update(
-        req.body,
-        { where: {id}, returning: true }
-    );
-    if(result[0] === 0) return res.sendStatus(404);
+    const result = await Product.update(req.body, {
+        where: { id },
+        returning: true,
+    });
+    if (result[0] === 0) return res.sendStatus(404);
     return res.json(result[1][0]);
 });
 
+const setImages = catchError(async (req, res) => {
+    const { id } = req.params; // product/:id/images
 
-const setImages = catchError(async(req,res)=>{
-    const {id} = req.params
+    const product = await Product.findByPk(id);
 
-    const product = await Product.findByPk(id)
+    await product.setProductImgs(req.body);
+    const images = await product.getProductImgs();
 
-    await product.setProductImgs([req.body])
-    const images = await product.getProductImgs()
-
-    return res.json(images)
-})
+    return res.json(images);
+});
 
 module.exports = {
     getAll,
@@ -60,5 +61,5 @@ module.exports = {
     getOne,
     remove,
     update,
-    setImages
-}
+    setImages,
+};
--- src/controllers/purchase.controllers.js
+++ src/controllers/purchase.controllers.js
@@ -1,37 +1,42 @@
-const catchError = require('../utils/catchError');
-const Purchase = require('../models/Purchase');
-const Product = require('../models/Product');
-const Cart = require('../models/Cart');
+const catchError = require("../utils/catchError");
+const Purchase = require("../models/Purchase");
+const Product = require("../models/Product");
+const Cart = require("../models/Cart");
+const ProductImg = require("../models/ProductImg");
+const Category = require("../models/Category");
 
-const getAll = catchError(async(req, res) => {
-    const userId = req.user.id
+const getAll = catchError(async (req, res) => {
+    const userId = req.user.id;
     const results = await Purchase.findAll({
-        where:{userId},
-        include:[Product]
-
+        where: { userId },
+        include: [
+            {
+                model: Product,
+                include: [Category, ProductImg],
+            },
+        ],
     });
     return res.json(results);
 });
 
-const create = catchError(async(req, res) => {
-    const userId = req.user.id
+const create = catchError(async (req, res) => {
+    const userId = req.user.id;
 
-    const cart = await Cart.findAll({ //cart is array
-        where:{userId},
-        attributes:["userId", "productId", "quantity"],
-        raw:true
-    })
-    
+    const cart = await Cart.findAll({
+        //cart is array
+        where: { userId },
+        attributes: ["userId", "productId", "quantity"],
+        raw: true,
+    });
+
     const result = await Purchase.bulkCreate(cart);
 
-    await Cart.destroy({where:{userId}})
+    await Cart.destroy({ where: { userId } });
 
     return res.status(201).json(result);
 });
 
-
-
 module.exports = {
     getAll,
-    create
-}
+    create,
+};
